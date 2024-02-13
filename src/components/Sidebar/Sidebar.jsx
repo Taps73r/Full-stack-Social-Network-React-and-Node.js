1ee88d49f48224e8ef4d0bfd10830c7b570dbd6d
Taps73r <povroznykmisha@gmail.com>
2024-02-13 22:44:59 +0200

    Adaptive

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -39,27 +39,27 @@ function Sidebar() {
 
       <div className="sidebar-mobile">
         <NavLink to="/profile" id="f-a">
-          <div className="first-item">
+          <div className="first-item_mobile">
             <span className="material-symbols-outlined">person</span>
           </div>
         </NavLink>
         <NavLink to="/dialogs">
-          <div className="sidebaritem">
+          <div className="sidebaritem_item">
             <span className="material-symbols-outlined">chat</span>
           </div>
         </NavLink>
         <NavLink to="/users">
-          <div className="sidebaritem">
+          <div className="sidebaritem_item">
             <span className="material-symbols-outlined">group_add</span>
           </div>
         </NavLink>
         <NavLink to="/news">
-          <div className="sidebaritem">
+          <div className="sidebaritem_item">
             <span className="material-symbols-outlined">newsmode</span>
           </div>
         </NavLink>
         <NavLink to="/settings" id="l-a">
-          <div className="last-item">
+          <div className="last-item_mobile">
             <span className="material-symbols-outlined">settings</span>
           </div>
         </NavLink>
