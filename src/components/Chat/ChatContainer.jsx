676204e9468bdf8c116acf8e6f189493b1b25c74
Taps73r <povroznykmisha@gmail.com>
2024-02-18 22:45:02 +0200

    add endpoints

--- src/components/Chat/ChatContainer.jsx
+++ src/components/Chat/ChatContainer.jsx
@@ -4,9 +4,13 @@ import axios from "axios";
 import { connect } from "react-redux";
 import { setIsFetching } from "../../redux/profile-reducer";
 import Preloader from "../common/Preloader/Preloader";
+import CreateChat from "../common/CreateChat/CreateChat";
+import { NavLink } from "react-router-dom";
+import "./Chat.css";
 
 function ChatContainer({ userId, isFetching, setIsFetching }) {
   const [chats, setChats] = useState([]);
+  const [followedUsers, setFollowedUsers] = useState(false);
 
   useEffect(() => {
     const fetchChats = async () => {
@@ -32,17 +36,63 @@ function ChatContainer({ userId, isFetching, setIsFetching }) {
 
     fetchChats();
   }, [setIsFetching, userId]);
+
+  const createChat = async (participantId) => {
+    const token = localStorage.getItem("token");
+    const partId = participantId;
+    try {
+      await axios
+        .post(
+          `http://localhost:3002/private-chats`,
+          { partId },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+        });
+    } catch (error) {
+      setIsFetching(false);
+      console.error("Помилка створення чату:", error);
+    }
+  };
   if (isFetching || !chats) {
     return <Preloader />;
   }
+  if (followedUsers === true) {
+    return (
+      <CreateChat
+        createChat={createChat}
+        followedUsers={followedUsers}
+        setFollowedUsers={setFollowedUsers}
+      />
+    );
+  }
+
   return (
     <div className="chat-page">
       <div className="chat-route">
         {chats.map((element) => {
-          return <div className="chat-list-block"></div>;
+          return (
+            <div className="chat-list-block" key={element.userId}>
+              <NavLink>
+                <img src={element.avatar} alt="" />
+                <p>{element.username}</p>
+              </NavLink>
+            </div>
+          );
         })}
         <div className="create-chat">
-            
+          <button
+            onClick={() => {
+              setFollowedUsers(true);
+            }}
+          >
+            Create Chat
+          </button>
         </div>
       </div>
       <Chat />
