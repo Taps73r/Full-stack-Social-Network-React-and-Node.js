f6682a95dd0ebcd8d7ff47f9ff4112fc707d6470
Taps73r <povroznykmisha@gmail.com>
2024-02-18 22:45:14 +0200

    add create chat

---
+++ src/components/common/CreateChat/CreateChat.jsx
@@ -0,0 +1,59 @@
+import axios from "axios";
+import "./CreateChat.css";
+import staticPhoto from "../../../photos/userstaticavatar.jpg";
+
+import React, { useEffect, useState } from "react";
+
+export default function CreateChat(props) {
+  const [subscriptions, setSubscriptions] = useState([]);
+  useEffect(() => {
+    async function fetchSubscriptions() {
+      const token = localStorage.getItem("token");
+      try {
+        const response = await axios.get(
+          "http://localhost:3002/subscriptions",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        console.log(response);
+        setSubscriptions(response.data);
+      } catch (error) {
+        console.error("Помилка при отриманні списку підписок:", error);
+      }
+    }
+    fetchSubscriptions();
+  }, []);
+  return (
+    <div className="Create-chat">
+      <button
+        className="back-btn"
+        onClick={() => {
+          props.setFollowedUsers(false);
+        }}
+      >
+        back
+      </button>
+      {subscriptions.map((sub) => {
+        return (
+          <div className="create-chat-block" key={sub.userId}>
+            <div className="create-chat-card">
+              <img src={sub.photo != null ? sub.photo : staticPhoto} alt="" />
+              <p>{sub.name}</p>
+            </div>
+            <button
+              className="create-chat-btn"
+              onClick={() => {
+                props.createChat(sub.userId);
+              }}
+            >
+              Create Chat
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
