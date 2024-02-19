2164fdce0983229e091d13cf05589b18f04b1704
Taps73r <povroznykmisha@gmail.com>
2024-02-19 14:04:50 +0200

    create chat mesage block

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,6 +1,35 @@
-function Chat(props) {
-  return <div className="chat">
+import axios from "axios";
+import { useEffect } from "react";
 
+function Chat(props) {
+  useEffect(() => {
+    const fetchChat = async (chatId) => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get(
+          `http://localhost:3002/private-chats/${chatId}/messages`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        console.log(response.data);
+      } catch (error) {
+        console.error("Помилка отримання повідомлень чата:", error);
+      }
+    };
+
+    if (props.selectedChat) {
+      fetchChat(props.selectedChat);
+    }
+  }, [props.selectedChat]);
+  return <div className="chat">
+    <div className="chat-messages"></div>
+    <div className="enter-message">
+      <textarea placeholder="Enter your message"></textarea>
+      <button>Send</button>
+    </div>
   </div>;
 }
 
