0f3c61b262969a22d9fbe4cf31d5831c524326bd
Taps73r <povroznykmisha@gmail.com>
2024-02-28 20:24:18 +0200

    update news

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,15 @@
-import { NavLink } from 'react-router-dom';
-import './Header.css';
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 function Header() {
-    return (
-        <div className='header'>
-            <div className='header-name'>
-                <NavLink to='/' id='h-d-name' >
-                    converso
-                </NavLink>
-            </div>
-        </div>
-    )
+  return (
+    <div className="header">
+      <div className="header-name">
+        <NavLink to="/news" id="h-d-name">
+          converso
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
