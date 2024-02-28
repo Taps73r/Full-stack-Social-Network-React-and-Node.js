f5afaea52a0a5b8144c43ab8b91d53f9448c8240
Taps73r <povroznykmisha@gmail.com>
2024-02-28 15:02:58 +0200

    update connect

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,28 +1,36 @@
 import UsersAPIComponent from "./UsersAPIComponent";
-import { setCurrentPage, setIsFetching, setTotalUsersCount, setUsersAC, toggleSubscription, updateSearchUserText } from "../../redux/users-reducer";
-import { connect } from 'react-redux';
+import {
+  setCurrentPage,
+  setIsFetching,
+  setTotalUsersCount,
+  setUsersAC,
+  toggleSubscription,
+  updateSearchUserText,
+} from "../../redux/users-reducer";
+import { connect } from "react-redux";
+import { setErrorMessage } from "../../redux/error-reducer";
 
 let mapUsersToProps = (state) => {
-    return {
-        usersList: state.usersInfo.users,
-        pageSize: state.usersInfo.pageSize,
-        totalUsersCount: state.usersInfo.totalUsersCount,
-        currentPage: state.usersInfo.currentPage,
-        newUserSearchText: state.usersInfo.newUserSearchText,
-        isFetching: state.usersInfo.isFetching,
-        userId: state.loginInfo.userId
-    }
-}
+  return {
+    usersList: state.usersInfo.users,
+    pageSize: state.usersInfo.pageSize,
+    totalUsersCount: state.usersInfo.totalUsersCount,
+    currentPage: state.usersInfo.currentPage,
+    newUserSearchText: state.usersInfo.newUserSearchText,
+    isFetching: state.usersInfo.isFetching,
+    userId: state.loginInfo.userId,
+    errorMessage: state.errorInfo.errorMessage,
+  };
+};
 
-let UsersContainer = connect(mapUsersToProps,
-    {
-        setUsers: setUsersAC,
-        setCurrentPage: setCurrentPage,
-        setTotalUsersCount,
-        updateSearchUserText,
-        setIsFetching,
-        toggleSubscription
-    })
-    (UsersAPIComponent);
+let UsersContainer = connect(mapUsersToProps, {
+  setUsers: setUsersAC,
+  setCurrentPage: setCurrentPage,
+  setTotalUsersCount,
+  updateSearchUserText,
+  setIsFetching,
+  toggleSubscription,
+  setErrorMessage,
+})(UsersAPIComponent);
 
 export default UsersContainer;
