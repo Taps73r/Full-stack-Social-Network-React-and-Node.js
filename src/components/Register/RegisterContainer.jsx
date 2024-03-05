7f194766b84e9f5d6bc20b61fa8f84b11900ebe4
Taps73r <povroznykmisha@gmail.com>
2024-03-05 16:59:59 +0200

    Add error handler to register component

--- src/components/Register/RegisterContainer.jsx
+++ src/components/Register/RegisterContainer.jsx
@@ -1,59 +1,69 @@
-import React from 'react';
-import Preloader from './../common/Preloader/Preloader';
-import Registration from './Register';
-import { connect } from 'react-redux';
-import { registerFailure, registerRequest, registerSuccess, registerUpdatePassText, registerUpdateUsernameText } from '../../redux/register-reducer';
-import axios from 'axios';
-import SuccessComponent from './SuccesComponent';
-import './Register.css';
+import React from "react";
+import Registration from "./Register";
+import { connect } from "react-redux";
+import {
+  registerRequest,
+  registerSuccess,
+  registerUpdatePassText,
+  registerUpdateUsernameText,
+} from "../../redux/register-reducer";
+import axios from "axios";
+import SuccessComponent from "./SuccesComponent";
+import "./Register.css";
+import { setErrorMessage } from "../../redux/error-reducer";
+import ErrorCatcherContainer from "../common/ErrorCatcher/ErrorCatcher";
 class RegisterContainer extends React.Component {
+  handleRegistration = () => {
+    this.props.registerRequest();
 
-    handleRegistration = () => {
-        this.props.registerRequest();
+    const username = this.props.username;
+    const password = this.props.password;
 
-        const username = this.props.username;
-        const password = this.props.password;
-
-        axios.post('http://localhost:3002/auth/register', { username, password })
-            .then((response) => {
-                this.props.registerSuccess();
-                // Виконайте додаткові дії для успішної реєстрації
-            })
-            .catch((error) => {
-                this.props.registerFailure(error);
-                // Виконайте додаткові дії для обробки помилки реєстрації
-            });
+    axios
+      .post("http://localhost:3002/auth/register", { username, password })
+      .then((response) => {
+        this.props.registerSuccess();
+      })
+      .catch((error) => {
+        this.props.setErrorMessage(error.response.data.message);
+      });
+  };
+  render() {
+    if (this.props.success) {
+      return <SuccessComponent />;
+    } else {
+      return (
+        <>
+          {this.props.errorMessage ? <ErrorCatcherContainer /> : <></>}
+          <Registration
+            username={this.props.username}
+            password={this.props.password}
+            onRegistration={this.handleRegistration}
+            registerPassText={this.props.registerUpdatePassText}
+            registerUsnameText={this.props.registerUpdateUsernameText}
+            error={this.props.error}
+          />
+        </>
+      );
     }
-    render() {
-        if (this.props.isLoading) {
-            return <Preloader />
-        } else if (this.props.success) {
-            return <SuccessComponent />
-        } else {
-            return (
-                <Registration
-                    username={this.props.username}
-                    password={this.props.password}
-                    onRegistration={this.handleRegistration}
-                    registerPassText={this.props.registerUpdatePassText}
-                    registerUsnameText={this.props.registerUpdateUsernameText}
-                    error={this.props.error}
-                />
-            );
-        }
-    }
+  }
 }
 
 let mapStateToProps = (state) => ({
-    isLoading: state.registerInfo.isLoading,
-    success: state.registerInfo.success,
-    error: state.registerInfo.error,
-    username: state.registerInfo.username,
-    password: state.registerInfo.password
-})
+  isLoading: state.registerInfo.isLoading,
+  success: state.registerInfo.success,
+  error: state.registerInfo.error,
+  username: state.registerInfo.username,
+  password: state.registerInfo.password,
+  errorMessage: state.errorInfo.errorMessage,
+});
 
 let RegisterContainerAPI = connect(mapStateToProps, {
-    registerRequest, registerFailure, registerSuccess, registerUpdatePassText, registerUpdateUsernameText
+  registerRequest,
+  registerSuccess,
+  registerUpdatePassText,
+  registerUpdateUsernameText,
+  setErrorMessage,
 })(RegisterContainer);
 
 export default RegisterContainerAPI;
